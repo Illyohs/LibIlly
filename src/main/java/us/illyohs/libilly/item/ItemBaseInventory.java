58b442639ccf758bffa45b5f4bbd5feabcdf5e70
Anthony Anderson <lordillyohs@gmail.com>
2015-11-21 03:02:04 -0500

    sigh
    Signed-off-by: Anthony Anderson <lordillyohs@gmail.com>

--- src/main/java/us/illyohs/libilly/item/ItemBaseInventory.java
+++ src/main/java/us/illyohs/libilly/item/ItemBaseInventory.java
@@ -2,12 +2,11 @@ package us.illyohs.libilly.item;
 
 import net.minecraft.creativetab.CreativeTabs;
 
-/**
- * Created by illyohs on 11/21/15.
- */
+@Deprecated
 public class ItemBaseInventory extends ItemBase {
 
     public ItemBaseInventory(String name, boolean hasSubtypes, CreativeTabs tabs) {
         super(name, hasSubtypes, tabs);
     }
+
 }
