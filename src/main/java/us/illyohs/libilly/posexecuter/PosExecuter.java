4ab1401a73ee80ce4073cab9f1949af70d296986
Anthony Anderson <lordillyohs@gmail.com>
2015-11-29 07:17:07 -0500

    Restart work on PosExecuters
    Signed-off-by: Anthony Anderson <lordillyohs@gmail.com>

--- src/main/java/us/illyohs/libilly/posexecuter/PosExecuter.java
+++ src/main/java/us/illyohs/libilly/posexecuter/PosExecuter.java
@@ -16,21 +16,31 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
  *  USA
  */
-package us.illyohs.libilly.posexecutor;
+package us.illyohs.libilly.posexecuter;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ResourceLocation;
 
 public class PosExecuter {
 
-    protected String  name;
-    protected int     posX;
-    protected int     posY;
-    protected int     posZ;
-    protected boolean doRenderer;
-    protected boolean isPersistent;
+    protected String           name;
+    protected int              posX;
+    protected int              posY;
+    protected int              posZ;
+    protected boolean          isPersistent;
+    protected ResourceLocation resource;
 
+    /**
+     * 
+     * @param name
+     */
+    public PosExecuter(String name) {
+        this.name = name;
+    }
+    
     /**
      * @param name
      * @param posX
@@ -39,13 +49,30 @@ public class PosExecuter {
      * @param dorenderer
      * @param isPersistent
      */
-    public PosExecuter(String name, int posX, int posY, int posZ, boolean dorenderer, boolean isPersistent) {
+    public PosExecuter(String name, int posX, int posY, int posZ, boolean dorenderer, boolean isPersistent, ResourceLocation location) {
         this.name = name;
         this.posX = posX;
         this.posY = posY;
         this.posZ = posZ;
-        this.doRenderer = dorenderer;
         this.isPersistent = isPersistent;
+        this.resource = location;
+    }
+    
+    /**
+     * 
+     * @param name
+     * @param pos
+     * @param dorenderer
+     * @param isPersistent
+     * @param location
+     */
+    public PosExecuter(String name, BlockPos pos, boolean dorenderer, boolean isPersistent, ResourceLocation location) {
+        this.name = name;
+        this.posX = pos.getX();
+        this.posY = pos.getY();
+        this.posZ = pos.getZ();
+        this.isPersistent = isPersistent;
+        this.resource = location;
     }
 
     public void executeOnRightClick(EntityPlayer player) {
@@ -96,20 +123,20 @@ public class PosExecuter {
         this.posZ = posZ;
     }
 
-    public boolean doRenderer() {
-        return doRenderer;
-    }
-
-    public void setDorenderer(boolean doRenderer) {
-        this.doRenderer = doRenderer;
-    }
-
     public boolean isPersistent() {
         return isPersistent;
     }
 
     public void setPersistent(boolean isPersistent) {
         this.isPersistent = isPersistent;
+    }
+    
+    public ResourceLocation getResource() {
+        return resource;
+    }
+    
+    public void setResource(ResourceLocation resource) {
+        this.resource = resource;
     }
 
 }
