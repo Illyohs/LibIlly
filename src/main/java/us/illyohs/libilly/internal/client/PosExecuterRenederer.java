b932840fda9113c7f7f40870a78886cc22ac5f82
Anthony Anderson <lordillyohs@gmail.com>
2015-12-06 02:08:21 -0500

    Switching workspaces
    Signed-off-by: Anthony Anderson <lordillyohs@gmail.com>

---
+++ src/main/java/us/illyohs/libilly/internal/client/PosExecuterRenederer.java
@@ -0,0 +1,19 @@
+package us.illyohs.libilly.internal.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraftforge.client.IRenderHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class PosExecuterRenederer extends IRenderHandler {
+
+	@Override
+	public void render(float ticks, WorldClient world, Minecraft mc) {
+		// TODO Auto-generated method stub
+		
+    }
+
+
+}
